e763a0beb855cf9ab9c6689327122c69e1f3cd4b
Russell <chpmnrssll@gmail.com>
2019-07-28 22:04:32 -0600

    Clean up

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* eslint class-methods-use-this: "off" */
 const jsYaml = require('js-yaml');
 const { flatten, unflatten } = require('flat');
 
@@ -17,9 +16,7 @@ class YamlTransformer {
       // only keys that end with '.image'
       .filter(key => key.lastIndexOf('.image') !== -1)
       .forEach(imageKey => {
-        // g-image src requires a relative path to the image, netlify-cms outputs
-        // an absolute path. Prepending '../../static' to the netlifys' public_folder
-        // gives the relative path from '/src/pages'
+        // prepending '../../static' to image path gives the relative path from '/src/pages'
         flatData[imageKey] = `../../static${flatData[imageKey]}`;
       });
 
