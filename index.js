8d4b3eea57acfa6bf32352d7168b90575018b950
Russell <chpmnrssll@gmail.com>
2019-08-06 00:01:04 -0600

    Added marked to process markdown inside of .yml files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const jsYaml = require('js-yaml');
 const { flatten, unflatten } = require('flat');
+const jsYaml = require('js-yaml');
+const marked = require('marked');
 
 class YamlTransformer {
   static mimeTypes() {
@@ -18,6 +19,13 @@ class YamlTransformer {
       .forEach(imageKey => {
         // prepending '../../static' to image path gives the relative path from '/src/pages'
         flatData[imageKey] = `../../static${flatData[imageKey]}`;
+      });
+
+    Object.keys(flatData)
+      // only keys that end with '.body'
+      .filter(key => key.lastIndexOf('.body') !== -1)
+      .forEach(bodyKey => {
+        flatData[bodyKey] = marked(flatData[bodyKey]);
       });
 
     data = unflatten(flatData);
